8edd2bc64f3ce85f4958b7129d24ea1cece646a0
Stéphane Mullings <stephane.mullings@ermeo.com>
2020-05-28 10:37:28 +0200

    Create ConfigurationTest file

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DependencyInjection;
+
+use Ermeo\RateLimitBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends TestCase
+{
+    /**
+     * @var Processor
+     */
+    private $processor;
+
+    public function setUp():void
+    {
+        $this->processor = new Processor();
+    }
+
+    private function getConfigs(array $configArray)
+    {
+        $configuration = new Configuration();
+
+        return $this->processor->processConfiguration($configuration, array($configArray));
+    }
+
+    public function testGetConfigTreeBuilder(): void
+    {
+        $configuration = $this->getConfigs(array());
+
+        $this->assertSame(array(), $configuration);
+    }
+}
