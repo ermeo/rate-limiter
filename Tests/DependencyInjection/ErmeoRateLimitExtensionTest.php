712716567477ee5bf477d30365785009f74efbc7
Stéphane Mullings <stephane.mullings@ermeo.com>
2020-05-29 10:12:50 +0200

    Create ErmeoRateLimitExtension and his unit test

---
+++ Tests/DependencyInjection/ErmeoRateLimitExtensionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DependencyInjection;
+
+use Ermeo\RateLimitBundle\DependencyInjection\ErmeoRateLimitExtension;
+use PHPUnit\Framework\TestCase;
+
+class ErmeoRateLimitExtensionTest extends TestCase
+{
+
+    public function testLoad()
+    {
+        $this->assertTrue(true);
+    }
+}
